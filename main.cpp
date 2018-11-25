99804b854743d31d5cb423b148742828d64f657b
Ank <a.vishnubhotla@ufl.edu>
2018-11-24 20:30:24 -0500

    main

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Page Rank
+//
+//  Created by Ankit  on 11/24/18.
+//  Copyright © 2018 blank. All rights reserved.
+//
+
+#include <iostream>
+#include <cstring>
+
+int main(int argc, const char * argv[]) {
+    int numConnections, powerIter = 0;
+    std::cin >> numConnections;
+    std::cin >> powerIter;
+    for(int i = 0; i < numConnections; i++) {
+        
+    }
+    return 0;
+}
